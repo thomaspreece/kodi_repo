a47c1ebabd5768a6170f015e48e001b622f481af
Thomas Preece <thomaspreece10@gmail.com>
2017-09-16 21:49:17 +0100

    Updated repo with latest submodule commits. Also Added module dependancies

--- runme.sh
+++ runme.sh
@@ -1,1 +1,1 @@
-./create_repository.py --datadir=repo-html plugin.video.redux/
+./create_repository.py --datadir=repo-html Addons/plugin.video.redux/ Addons/script.module.cachecontrol Addons/script.module.lockfile
